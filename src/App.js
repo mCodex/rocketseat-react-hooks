65756a406e79c3d5e9feb86e15523e986a14cd35
Mateus Andrade <mat.andrade@live.com>
2020-03-08 17:00:32 -0300

    added useState example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-    return <h1>Hello World</h1>;
+    const [tech, setTech] = useState(['ReactJS', 'React Native']);
+
+    const [newTech, setNewTech] = useState('');
+
+    const handleAdd = () => setTech(prevState => [...prevState, newTech]);
+
+    return (
+        <>
+            <ul>
+                {tech.map(t => (
+                    <li key={t}>{t}</li>
+                ))}
+            </ul>
+            <input type="text" onChange={e => setNewTech(e.target.value)} />
+            <button type="button" onClick={handleAdd}>
+                Adicionar
+            </button>
+        </>
+    );
 };
 
 export default App;
