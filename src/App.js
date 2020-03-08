2a20aeb96fbb67f2958fb62b4f73b857f6245c98
Mateus Andrade <mat.andrade@live.com>
2020-03-08 17:43:40 -0300

    added useMemo and useCallback example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 const App = () => {
     const [tech, setTech] = useState([]);
     const [newTech, setNewTech] = useState('');
+
+    const techSize = useMemo(() => tech.length, [tech]);
 
     useEffect(() => {
         const techsFromLS = localStorage.getItem('tech');
@@ -16,7 +18,9 @@ const App = () => {
         localStorage.setItem('tech', JSON.stringify(tech));
     }, [tech]);
 
-    const handleAdd = () => setTech(prevState => [...prevState, newTech]);
+    const handleAdd = useCallback(() => {
+        setTech(prevState => [...prevState, newTech]);
+    }, [newTech]);
 
     return (
         <>
@@ -25,6 +29,8 @@ const App = () => {
                     <li key={t}>{t}</li>
                 ))}
             </ul>
+            <strong>Você tem {techSize} tecnologias</strong>
+            <br />
             <input type="text" onChange={e => setNewTech(e.target.value)} />
             <button type="button" onClick={handleAdd}>
                 Adicionar
